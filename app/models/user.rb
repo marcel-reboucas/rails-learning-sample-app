2bdc28e2e293b64a1d4543b0b7e4680fa11a60a9
Marcel Rebouças <mscreboucas@gmail.com>
2016-11-04 11:47:52 -0300

    Finishes validating the activation links

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,45 +14,58 @@ class User < ApplicationRecord
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
 
-  	# Returns the hash digest of the given string.
-  	def User.digest(string)
-    	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
-    	BCrypt::Password.create(string, cost: cost)
-  	end
+    BCrypt::Password.create(string, cost: cost)
+  end
 
-  	# Returns a randomly generated token
-  	def User.new_token
-  		SecureRandom.urlsafe_base64
-  	end
+  # Returns a randomly generated token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 
-  	def remember
-    	self.remember_token = User.new_token
-    	update_attribute(:remember_digest, User.digest(remember_token))
-  	end	
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end 
 
-    # Forgets a user in a persistent session.
-    def forget
-      update_attribute(:remember_digest, nil)
+  # Forgets a user in a persistent session.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end 
+
+
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+    
+  private 
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
     end
 
-  	# Returns true if the given token matches the digest.
-  	def authenticated?(remember_token)
-      return false if remember_digest.nil?
-    	BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  	end	
-
-    private 
-
-      # Converts email to all lower-case.
-      def downcase_email
-        self.email = email.downcase
-      end
-
-      # Creates and assigns the activation token and digest.
-      def create_activation_digest
-        self.activation_token = User.new_token
-        self.activation_digest = User.digest(activation_token)
-      end
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 end
