3a0b9efe39212a26222f9100817ea56fa1a0897d
Marcel Rebouças <mscreboucas@gmail.com>
2016-11-06 12:32:36 -0300

    Adds following posts to the user's feed.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -95,4 +95,22 @@ class UserTest < ActiveSupport::TestCase
     marcel.unfollow(archer)
     assert_not marcel.following?(archer)
   end
+
+  test "feed should have the right posts" do
+    marcel = users(:marcel)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert marcel.feed.include?(post_following)
+    end
+    # Posts from self
+    marcel.microposts.each do |post_self|
+      assert marcel.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not marcel.feed.include?(post_unfollowed)
+    end
+  end
  end
